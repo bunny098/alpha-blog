10304689c01a38e32dc8669f149a06cff90561c8
Akash sahu <sahuakash098@gmail.com>
2018-03-22 12:03:53 +0000

    create new action routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
- validates :title, presence: true, length: {minimum:5 ,maximum:20}
- validates :description, presence: true, length: {minimum:20, maximum:100}
+ validates :title, presence: true, length: {minimum:5 ,maximum:200}
+ validates :description, presence: true, length: {minimum:5, maximum:100}
 
 end
