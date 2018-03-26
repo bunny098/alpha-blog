740d3609b2b480078c5b0acd5740a7aeac16dba0
Akash sahu <sahuakash098@gmail.com>
2018-03-26 12:36:13 +0000

    create show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
-class ArticlesController < ApplicationController
+class ArticlesController< ApplicationController
   def new
     @article=Article.new
   end
+  
   def create
     @article=Article.new(article_params)
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was Sucessfully Created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+  
+  def show
+    @article=Article.find(params[:id])
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :description)
