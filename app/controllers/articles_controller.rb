f6d37030e1f3d827710402097d85eb072e598fb5
Akash sahu <sahuakash098@gmail.com>
2018-03-27 08:51:24 +0000

    CRU Only

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController< ApplicationController
+  
+  def index
+      @articles_all=Article.all
+  end
+  
   def new
     @article=Article.new
   end
@@ -6,7 +11,7 @@ class ArticlesController< ApplicationController
   def create
     @article=Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was Sucessfully Created"
+      flash[:notice] = "Article was sucessfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -15,6 +20,20 @@ class ArticlesController< ApplicationController
   
   def show
     @article=Article.find(params[:id])
+  end
+  
+  def edit
+    @article=Article.find(params[:id])
+  end
+  
+  def update
+  @article=Article.find(params[:id])
+  if @article.update(article_params)
+   flash[:notice] = "Article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
