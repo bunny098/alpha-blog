757d15ff044062f7325f063177fbbccc9fed092d
Akash sahu <sahuakash098@gmail.com>
2018-05-25 11:13:59 +0000

    Add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController< ApplicationController
   
   def index
-      @articles_all=Article.all
+      @articles_all=Article.paginate(page: params[:page], per_page:5)
   end
   
   def new
